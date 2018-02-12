42565bfcb130a3105c40506c897209c4efa14605
Igor <igorlipko@75gmail.com>
2018-02-13 00:02:39 +0300

    Behavioral pattern - Chain of responsibility

--- src/structural/Flyweight/Program.java
+++ src/structural/Flyweight/Program.java
@@ -7,9 +7,9 @@ public class Program {
 
     public static void main(String[] args) {
         DeveloperFactory developerFactory = new DeveloperFactory();
-        
+
         List<Developer> developers = new ArrayList<>();
-        
+
         developers.add(developerFactory.getDeveloperBySpeciality("java"));
         developers.add(developerFactory.getDeveloperBySpeciality("java"));
         developers.add(developerFactory.getDeveloperBySpeciality("java"));
